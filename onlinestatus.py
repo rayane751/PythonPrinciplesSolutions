89dc5ab3b5b4440918601b1fa935d6c44cc8d671
rayane751 <94043808+rayane751@users.noreply.github.com>
2023-05-08 14:00:29 +0200

    Update onlinestatus.py

--- onlinestatus.py
+++ onlinestatus.py
@@ -1,13 +1,15 @@
+#Dictionary we will use
 statuses = {
     "Alice": "online",
     "Bob": "offline",
     "Eve": "online",
     }
-   
+
+#Initialisation of the function 
 def online_count (statuses):
-    nb_pers = 0   
-    for occurence,number in statuses.items():
-        if number == "online":
-            nb_pers+=1
+    nb_pers = 0  #variable where we store the number of occurences 
+    for occurence,number in statuses.items(): #for each element of the dictionary, we store each key in occurence and the value of the key in number
+        if number == "online": #we check if the value in number is "online" 
+            nb_pers+=1 #we add 1 to the last value of this variable in order to count
             
-    return nb_pers  
+    return nb_pers  #and return the number of users online
