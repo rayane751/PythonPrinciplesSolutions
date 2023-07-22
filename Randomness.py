54bb4b8509b47886dc7b7c7d947d87baac97ab6b
rayane751 <94043808+rayane751@users.noreply.github.com>
2023-07-23 01:59:10 +0200

    Update Randomness.py
    Added comments

--- Randomness.py
+++ Randomness.py
@@ -1,4 +1,5 @@
-import random 
-def random_number():
-    number = random.randrange(1,101)
-    return number
+import random #Importing random module to generate random numbers 
+
+def random_number(): #Start of the function
+    number = random.randrange(1,101) # We create a variable where we store the random number in a range between 1 and 101 to have 100 numbers
+    return number # We return the value of the variable which is the randomly generated number
